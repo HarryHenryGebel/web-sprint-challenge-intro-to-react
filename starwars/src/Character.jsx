0cba20a20eb04d71f2d5dcab787c28776330ca14
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-17 12:43:52 -0400

    Stretch goal - make Character more complicated

--- starwars/src/Character.jsx
+++ starwars/src/Character.jsx
@@ -5,9 +5,38 @@ const H3 = styled.h3`
   color: teal;
 `;
 
+const CharacterDiv = styled.div`
+  margin: 1rem;
+`;
+
+const DataDiv = styled.div`
+  margin: 0.25rem;
+  margin-left: 2rem;
+`;
+
+const DataItem = styled.p`
+  color: black;
+`;
+
 export default function Character(props) {
-  const {character} = props;
+  const {character} = props,
+        {name, birth_year, gender} = character;
+
   return (
-    <H3>{character.name}</H3>
+    <CharacterDiv>
+      <H3>{name}</H3>
+      <CharacterInfo birth_year={birth_year} gender={gender}/>
+    </CharacterDiv>
   );
+}
+
+function CharacterInfo(props) {
+  const {birth_year, gender} = props;
+
+  return (
+    <DataDiv>
+      <DataItem>{birth_year}</DataItem>
+      <DataItem>{gender}</DataItem>
+    </DataDiv>
+);
 }
