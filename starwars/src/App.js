7e5d69b7604847aafbbb85170488bdce496fee08
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-20 01:37:59 -0400

    Upgrade easier-requests, cleanup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import {requester} from 'easier-requests';
+import requester from 'easier-requests';
 import './App.css';
 import Character from './Character';
 import {extractData} from './helpers';
-
 
 function getCharacters(setCharacters) {
   // holds each new copy of character array
@@ -15,12 +14,10 @@ function getCharacters(setCharacters) {
     await requester.get(page, id);
 
     const response = requester.response(id).data;
-    console.log(response);
 
     const extractedResults = response.results.map(extractData);
     characters = [...characters].concat(extractedResults);
     setCharacters(characters);
-    console.log(characters);
 
     if (response.next)
       _getCharacters(setCharacters, response.next);
